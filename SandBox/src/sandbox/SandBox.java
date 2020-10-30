221f3792695301147af87bb03b76fe5c442d60c3
bowman <bowman@phobos>
2020-10-30 13:24:49 +0100

    First publication on GitHub

--- SandBox/src/sandbox/SandBox.java
+++ SandBox/src/sandbox/SandBox.java
@@ -1,33 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sandbox;
+x-special/nautilus-clipboard
+cut
+file:///home/bowman/Documents/He2B/INT1/INR/Exams/09/2007-08.pdf
+file:///home/bowman/Documents/He2B/INT1/INR/Exams/09/2008-08.pdf
+file:///home/bowman/Documents/He2B/INT1/INR/Exams/09/2009-08.pdf
+file:///home/bowman/Documents/He2B/INT1/INR/Exams/09/2016-08.pdf
+file:///home/bowman/Documents/He2B/INT1/INR/Exams/09/2019-08.pdf
 
-import FvpLib.Utils;
-
-/**
- *
- * @author bowman
- */
-
-public class SandBox {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        double val[] = {
-            333.97872,
-            1343.44,
-            999999.1333342
-       
-        };
-        
-        for (int i=0; i<val.length; i++) {
-            System.out.println(Utils.dotAligned(8, 2, val[i]));
-        }  
-    }
-    
-}
